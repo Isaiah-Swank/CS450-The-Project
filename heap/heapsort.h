cac96e70e3063cffb48ef91935f4e9b0824746c9
Isaiah-Swank <iws8@nau.edu>
2024-12-09 21:03:54 -0700

    heap fixes

--- heap/heapsort.h
+++ heap/heapsort.h
@@ -2,21 +2,9 @@
 #define HEAPSORT_H
 
 #include <stddef.h> // For size_t
-#include <stdio.h>  // For debugging or general output
+#include <stdio.h>
 
-/**
- * Function to perform heapsort on an array.
- * @param arr Pointer to the array to be sorted.
- * @param n Size of the array.
- */
 void heapsort(int *arr, int n);
-
-/**
- * Helper function to maintain heap property.
- * @param arr Pointer to the array.
- * @param n Size of the heap.
- * @param i Index of the current node.
- */
 void heapify(int *arr, int n, int i);
 
 #endif // HEAPSORT_H
