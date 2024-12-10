e902af66f0792a0c2751751aca10d343a206f270
Isaiah-Swank <iws8@nau.edu>
2024-12-09 18:49:32 -0700

    Added linked list

---
+++ linked-list/TestLinkedlist.cpp
@@ -0,0 +1,28 @@
+#include <deepstate/DeepState.hpp>
+extern "C" {
+    #include "linked_list.c"
+}
+
+using namespace deepstate;
+
+TEST(LinkedList, BasicOperations) {
+    Node* head = NULL;
+    int size = DeepState_IntInRange(1, 100);
+
+    for (int i = 0; i < size; i++) {
+        int value = DeepState_Int();
+        head = insertNode(head, value);
+        ASSERT(findNode(head, value) != NULL);
+    }
+
+    int target = DeepState_Int();
+    Node* result = findNode(head, target);
+
+    if (result != NULL) {
+        ASSERT(result->data == target);
+    } else {
+        LOG(TRACE) << "Node with data " << target << " not found.";
+    }
+
+    freeList(head);
+}
---
+++ linked-list/linked-list.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+Node* insertNode(Node* head, int data) {
+    Node* newNode = createNode(data);
+    newNode->next = head;
+    return newNode;
+}
+
+Node* findNode(Node* head, int target) {
+    while (head != NULL) {
+        if (head->data == target) return head;
+        head = head->next;
+    }
+    return NULL;
+}
+
+void freeList(Node* head) {
+    Node* temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
